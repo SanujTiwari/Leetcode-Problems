57a2e4d8e8be1111c9c188f856009e4c4a499760
Sanuj Tiwari <145697474+SanujTiwari@users.noreply.github.com>
2026-08-25 23:34:40 +0530

    Time: 2 ms (68.2%), Space: 45.5 MB (38.11%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -2,18 +2,13 @@ class Solution {
     public int missingMultiple(int[] nums, int k) {
         Set<Integer> set = new HashSet<>();
         
-        // Add all elements of nums to a HashSet for O(1) lookups
         for (int num : nums) {
             set.add(num);
         }
-        
-        // Check multiples of k starting from 1 * k
         int multiple = k;
         while (set.contains(multiple)) {
             multiple += k;
         }
-        
-        // Return the first multiple not found in the set
         return multiple;
     }
 }
